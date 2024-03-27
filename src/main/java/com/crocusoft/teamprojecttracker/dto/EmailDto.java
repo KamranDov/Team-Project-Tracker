88e3779c7a15fb4ce69553dcfdde0b23a2aa216c
Kamran <axmedovkamran19@gmail.com>
2024-03-27 12:40:33 +0400

    ADDED: database connections made dynamic

--- src/main/java/com/crocusoft/teamprojecttracker/dto/EmailDto.java
+++ src/main/java/com/crocusoft/teamprojecttracker/dto/EmailDto.java
@@ -1,7 +1,10 @@
 package com.crocusoft.teamprojecttracker.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +15,11 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class EmailDto {
 
-    @NotBlank(message = "Email cannot be blank")
+    @NotNull
+    @JsonProperty("email")
     @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be blank")
+    @Pattern(regexp = ".+@crocusoft\\.com$", message = "Email must end with @crocusoft.com.")
     String email;
     String subject;
     String message;
